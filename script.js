e53525e7f06caeaec3c916b1f39533040c886d9d
JoshyOnMars2 <101261710+JoshyOnMars2@users.noreply.github.com>
2022-03-09 12:34:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let x = 0
-let clicker = 1
+let energy = 0
+let energyPc = 1
 let cost = 25
 // if the key value pair exists, we set x equal to the stored value
 // else, set x to 0
 document.getElementById('btn').addEventListener("click", iterate)
 document.getElementById('upgradebtn').addEventListener("click", getUpgrade)
-document.getElementById("counter").innerHTML = x
-document.getElementById("epc").innerHTML = clicker
+document.getElementById("counter").innerHTML = energy
+document.getElementById("epc").innerHTML = energyPc
 document.getElementById("cost").innerHTML = cost
 function iterate(){
-    x += clicker
-    console.log(clicker,x)
-    document.getElementById("counter").innerHTML = x
+    energy += energyPc
+    console.log(energyPc,energy)
+    document.getElementById("counter").innerHTML = energy
 }
 
 function getUpgrade(){
-  if (x >= cost) {
-    clicker += 1
-    x = x - cost
+  if (energy >= cost) {
+    energyPc += 1
+    energy = energy - cost
     cost = Math.round(cost * 1.05)
     console.log("Item brought.")
-    document.getElementById("counter").innerHTML = x
-    document.getElementById("epc").innerHTML = clicker
+    document.getElementById("counter").innerHTML = energy
+    document.getElementById("epc").innerHTML = energyPc
     document.getElementById("cost").innerHTML = cost
   } else {
     console.log("No..")
