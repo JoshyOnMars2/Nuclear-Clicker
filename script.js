466a4d44366406a6f7d80bafca2af283157bfddf
JoshyOnMars2 <101261710+JoshyOnMars2@users.noreply.github.com>
2022-03-10 09:44:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 let energy = 0
 let energyPc = 1
+let energyPs = 0
 let cost = 25
 let cost2 = 100
+let cost3 = 25
 // if the key value pair exists, we set x equal to the stored value
 // else, set x to 0
 document.getElementById('btn').addEventListener("click", iterate)
 document.getElementById('upgradebtn').addEventListener("click", getUpgrade)
 document.getElementById('upgradebtn2').addEventListener("click", getUpgrade2)
+document.getElementById('upgradebtn3').addEventListener("click", getUpgrade3)
 
 register()
 function register(){
     document.getElementById("counter").innerHTML = energy
     document.getElementById("epc").innerHTML = energyPc
+    document.getElementById("eps").innerHTML = energyPs
     document.getElementById("cost").innerHTML = cost
     document.getElementById("cost2").innerHTML = cost2
+    document.getElementById("cost3").innerHTML = cost3
 }
 
 function iterate(){
@@ -46,9 +51,22 @@ function getUpgrade2(){
   }
 }
 
+function getUpgrade3(){
+  if (energy >= cost3) {
+    energyPs += 1
+    energy = energy - cost3
+    cost3 = Math.round(cost3 * 1.05)
+    console.log("Item brought.")
+    register()
+  } else {
+    console.log("No..")
+  }
+}
+
 window.setInterval(function(){
-    energy += 1	
+    energy += energyPs
     document.getElementById("counter").innerHTML = energy
+    document.getElementById("eps").innerHTML = energyPs
 }, 1000);
 
 //Hi
